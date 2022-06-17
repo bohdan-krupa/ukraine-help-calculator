873cd43ca30fc558ccfe505ad577f476143935a0
Bohdan <bodya.save.dev@gmail.com>
2022-06-18 00:33:39 +0300

    test

--- backend/get_items/app.py
+++ backend/get_items/app.py
@@ -4,8 +4,5 @@ import json
 def lambda_handler(event, _):
     return {
         "statusCode": 200,
-        "body": json.dumps(
-            ['item1',
-            'item2','item3']
-        ),
+        "body": json.dumps(['item1', 'item2','item3']),
     }
