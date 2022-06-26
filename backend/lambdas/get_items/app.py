bd62153f00c76561e55b0f9fa95aa55baf17162c
Bohdan <bodya.save.dev@gmail.com>
2022-06-27 00:53:52 +0300

    backend done

--- backend/lambdas/get_items/app.py
+++ backend/lambdas/get_items/app.py
@@ -1,8 +1,12 @@
 import json
+from db import DB
 
 
 def lambda_handler(event, _):
+    items_type = event["pathParameters"]["type"]
+    items = DB.get_items(items_type)
+
     return {
         "statusCode": 200,
-        "body": json.dumps(["item1", "item2", "item3"]),
+        "body": json.dumps(items),
     }
